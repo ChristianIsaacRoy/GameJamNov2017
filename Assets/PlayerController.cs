99069cc5418c6307c225d1c9e5274a02158f9555
RektSouls <mcadams.parker@gmail.com>
2017-11-17 22:09:28 -0700

    Finished player movement.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,41 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed =10;
-    private Rigidbody2D rigidBody
+    public float speed = 0 ;
     private bool Up;
     private bool Down;
     private bool Left;
-    private bool Righ;
+    private bool Right;
+
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode rightKey;
+    public KeyCode leftKey;
+
+    public GameObject wallPrefab;
+
+    Collider2D wall;
 
 
     private void Awake()
     {
-        rigidbody = GetComponent.rigidbody2D();
+   
     }
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-     Up = Input.GetButtonDown("D-Pad Up");
-     Down = Input.GetButtonDown("D-Pad Down");
-     Left = Input.GetButtonDown("D-Pad Left");
-     Right = Input.GetButtonDown("D-Pad Right");
-}
-
-    void PlayerInput()
-    {
-        if (Down = true)
-        {
-            rigidbody.
-        }
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
 
     }
 
+    // Update is called once per frame
+    void Update() {
+
+
+      if (Input.GetAxis("Vertical" )< 0)
+        {
+            
+            GetComponent<Rigidbody2D>().velocity = -Vector2.up * speed;
+        }
+      else if (Input.GetAxis("Vertical" )> 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        }
+      else if(Input.GetAxis("Horizontal") > 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        }
+      else if(Input.GetAxis("Horizontal") < 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = -Vector2.right * speed;
+        }
+    }
+
+
+
+    void spawnWall()
+    {
+        GameObject g = (GameObject)Instantiate(wallPrefab, transform.position, Quaternion.identity);
+        wall = g.GetComponent<Collider2D>();
+    }
 
 
 
