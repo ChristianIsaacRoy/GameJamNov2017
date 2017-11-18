b5de2ceb39c9ebae378dae1b23a88d7faade4d80
Christian <christian.50.roy@gmail.com>
2017-11-17 22:46:33 -0700

    Falling Squares, Kill Colliders, and Score Counting!

---
+++ Assets/FallingSquare.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class FallingSquare : MonoBehaviour
+{
+    public float timeTillDeath;
+    public float fallSpeed;
+
+    private float timeAlive;
+    private bool falling;
+
+    private void Awake()
+    {
+        falling = false;
+        timeAlive = 0;
+        transform.DOShakePosition(1f, .1f).OnComplete(() => Fall());
+    }
+	
+	private void Update()
+    {
+        if (falling)
+        {
+            timeAlive += Time.deltaTime;
+            transform.position += Vector3.down * fallSpeed;
+        }
+
+        if (timeAlive >= timeTillDeath)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void Fall()
+    {
+        falling = true;
+    }
+}
